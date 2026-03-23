758277486a85f611833049ac353da53ab01881e1
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-03-23 15:57:38 -0300

    Register RouterLink in App component
    Import RouterLink from @angular/router and add it to the App component's imports array (rotas/src/app/app.ts) so the template can use the RouterLink directive for in-app navigation.

--- rotas/src/app/app.ts
+++ rotas/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Resenha } from './resenha/resenha';
 import { Tropa } from './tropa/tropa';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Resenha,Tropa],
+  imports: [RouterOutlet, Resenha,Tropa, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
